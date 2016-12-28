830be3cf372c68758c27fb384e88f4ecf77a0d3e
Neitsab <bastienthefly@live.fr>
2016-12-28 10:15:58 +0100

    Add files via upload

---
+++ pageAvis.php
@@ -0,0 +1,40 @@
+<?php include('header.php') ?>
+    <nav>
+      <ul>
+        <li>
+          <a href="#">Particuliers</a>
+        </li>
+        <li>
+          <a href="#">Professionnels</a>
+        </li>
+        <li>
+          <a href="villes.php">Nos villes</a>
+        </li>
+        <li>
+          <a href="#">Nous recrutons</a>
+        </li>
+      </ul>
+    </nav>
+    <div class="img">
+      <img src="img/1.jpg" alt="">
+      <img src="img/2.jpg" alt="">
+      <img src="img/3.jpg" alt="">
+    </div>
+    <main>
+      <h1>Votre avis nous intéresse</h1>
+      <form class="avis" action="pageAvis.html" method="post">
+        <div class="form">
+          <label class="inf">&#9998;Informations :</label><br>
+          <form class="infos" action="pageAvis.html" method="post">
+            <input type="text" name="" value="" placeholder="Nom*"><br>
+            <input type="text" name="" value="" placeholder="Prénom*"><br>
+            <input type="number" name="" value="" placeholder="Téléphone*"><br>
+            <input type="email" name="" value="" placeholder="Email*"><br>
+            <textarea placeholder="Message*"></textarea><br>
+            <input class="button" type="button" name="" value="Envoyer">
+          </form>
+          <p>*Champs devant obligatoirement être renseignés</p>
+        </div>
+      </form>
+    </main>
+<?php include('footer.php') ?>
